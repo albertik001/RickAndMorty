a340c1893a2491c7196b57686e904a3acbca708d
albertik001 <albert.nurlanovv@mail.ru>
2022-08-10 01:51:29 +0600

    Kotlin Gradle data

---
+++ data/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    // Application
+    id(libs.plugins.agp.library.get().pluginId)
+
+    // Kotlin
+    id("kotlin-android")
+
+    // Kapt
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdk = config.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = config.versions.minSdk.get().toInt()
+        targetSdk = config.versions.targetSdk.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName(config.versions.releaseBuildType.get()) {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = options.versions.kotlinJvmTargetOptions.get()
+    }
+}
+
+dependencies {
+
+    api(project(":domain"))
+
+    // Retrofit
+    implementation(libs.bundles.retrofit)
+
+    // OkHttp
+    implementation(libs.bundles.okHttp)
+
+    // Room
+    api(libs.bundles.room)
+    kapt(libs.room.compiler)
+
+    //Paging 3
+    api(libs.paging.paging)
+
+
+}
