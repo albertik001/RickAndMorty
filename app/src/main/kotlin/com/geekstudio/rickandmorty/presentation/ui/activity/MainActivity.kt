3c19a032d21841f08c477646dcced70413f82fcc
albertik001 <albert.nurlanovv@mail.ru>
2022-08-10 18:40:17 +0600

    Added new main activity

--- app/src/main/kotlin/com/geekstudio/rickandmorty/presentation/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/com/geekstudio/rickandmorty/presentation/ui/activity/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geekstudio.rickandmorty.R
 import com.geekstudio.rickandmorty.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private val binding by viewBinding(ActivityMainBinding::bind)
 
