8f64d3e7c3778ab87d2808c1af24c5ee37d4175a
albertik001 <albert.nurlanovv@mail.ru>
2022-08-10 19:13:11 +0600

    Added charactersdto via kotlin to json file libu

---
+++ data/src/main/kotlin/com/geekstudio/rickandmorty/data/remote/dtos/CharactersDto.kt
@@ -0,0 +1,63 @@
+package com.geekstudio.rickandmorty.data.remote.dtos
+
+
+import com.google.gson.annotations.SerializedName
+
+data class CharactersDto(
+    @SerializedName("info")
+    val info: Info?,
+    @SerializedName("results")
+    val results: List<Result?>?
+)
+
+data class Info(
+    @SerializedName("count")
+    val count: Int?,
+    @SerializedName("pages")
+    val pages: Int?,
+    @SerializedName("next")
+    val next: String?,
+    @SerializedName("prev")
+    val prev: Any?
+)
+
+data class Location(
+    @SerializedName("name")
+    val name: String?,
+    @SerializedName("url")
+    val url: String?
+)
+
+data class Origin(
+    @SerializedName("name")
+    val name: String?,
+    @SerializedName("url")
+    val url: String?
+)
+
+data class Result(
+    @SerializedName("id")
+    val id: Int?,
+    @SerializedName("name")
+    val name: String?,
+    @SerializedName("status")
+    val status: String?,
+    @SerializedName("species")
+    val species: String?,
+    @SerializedName("type")
+    val type: String?,
+    @SerializedName("gender")
+    val gender: String?,
+    @SerializedName("origin")
+    val origin: Origin?,
+    @SerializedName("location")
+    val location: Location?,
+    @SerializedName("image")
+    val image: String?,
+    @SerializedName("episode")
+    val episode: List<String?>?,
+    @SerializedName("url")
+    val url: String?,
+    @SerializedName("created")
+    val created: String?
+)
