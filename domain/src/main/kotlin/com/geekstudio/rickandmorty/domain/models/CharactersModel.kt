da6898118e165be42f0e6cefaec94c71190ffb76
albertik001 <albert.nurlanovv@mail.ru>
2022-08-12 17:18:00 +0600

    added Character model domain

---
+++ domain/src/main/kotlin/com/geekstudio/rickandmorty/domain/models/CharactersModel.kt
@@ -0,0 +1,38 @@
+package com.geekstudio.rickandmorty.domain.models
+
+data class RickAndMortyResponseModel(
+    val info: InfoModel?,
+    val results: List<CharactersModel>
+)
+
+data class InfoModel(
+    val count: Int?,
+    val pages: Int?,
+    val next: String?,
+    val prev: Any?
+)
+
+data class LocationModel(
+    val name: String?,
+    val url: String?
+)
+
+data class OriginModel(
+    val name: String?,
+    val url: String?
+)
+
+data class CharactersModel(
+    val id: Int?,
+    val name: String?,
+    val status: String?,
+    val species: String?,
+    val type: String?,
+    val gender: String?,
+    val origin: OriginModel?,
+    val location: LocationModel?,
+    val image: String?,
+    val episode: List<String?>?,
+    val url: String?,
+    val created: String?
+)
