7f7d09808c1af68f41bc50f7bb5cf88209814735
albertik001 <albert.nurlanovv@mail.ru>
2022-08-12 17:16:47 +0600

    wrote a request for characters

--- app/src/main/kotlin/com/geekstudio/rickandmorty/presentation/ui/fragments/characters/CharactersViewModel.kt
+++ app/src/main/kotlin/com/geekstudio/rickandmorty/presentation/ui/fragments/characters/CharactersViewModel.kt
@@ -1,9 +1,19 @@
 package com.geekstudio.rickandmorty.presentation.ui.fragments.characters
 
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.geekstudio.rickandmorty.core.base.BaseViewModel
+import com.geekstudio.rickandmorty.domain.usecases.FetchPagedCharactersUseCase
+import com.geekstudio.rickandmorty.presentation.models.toUI
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class CharactersViewModel @Inject constructor() : BaseViewModel() {
+class CharactersViewModel @Inject constructor(
+    private val fetchPagedCharactersUseCase: FetchPagedCharactersUseCase
+) : BaseViewModel() {
+
+    fun fetchPagedCharacters() = fetchPagedCharactersUseCase().gatherPagingRequest { it.toUI() }
+
+
 }
