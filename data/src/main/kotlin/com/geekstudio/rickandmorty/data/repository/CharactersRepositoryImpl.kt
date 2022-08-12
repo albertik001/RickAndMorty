e788ed35257dd6ddb39e27edb2d79e65e075f8d3
albertik001 <albert.nurlanovv@mail.ru>
2022-08-12 17:21:54 +0600

    added CharacterRepositoryImpl + code wrote a request characters

---
+++ data/src/main/kotlin/com/geekstudio/rickandmorty/data/repository/CharactersRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.geekstudio.rickandmorty.data.repository
+
+import androidx.paging.map
+import com.geekstudio.rickandmorty.data.base.BaseRepository
+import com.geekstudio.rickandmorty.data.remote.apiservices.CharactersApi
+import com.geekstudio.rickandmorty.data.remote.dtos.toDomain
+import com.geekstudio.rickandmorty.data.remote.pagingsource.CharactersPagingSource
+import com.geekstudio.rickandmorty.domain.repository.CharactersRepository
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class CharactersRepositoryImpl @Inject constructor(private val charactersApi: CharactersApi) :
+    BaseRepository(), CharactersRepository {
+
+    override fun fetchPagedCharacters() =
+        doPagingRequest(CharactersPagingSource(charactersApi)).map { pagingData ->
+            pagingData.map {
+                it.toDomain()
+            }
+        }
+}
+
+
