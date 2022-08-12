94ca8a3f87eed4fa279fa0270872acd31dc02163
albertik001 <albert.nurlanovv@mail.ru>
2022-08-12 17:14:10 +0600

    fix BaseDiffModel.kt and BaseDiffUtil.kt

--- app/src/main/kotlin/com/geekstudio/rickandmorty/core/base/BaseDiffUtil.kt
+++ app/src/main/kotlin/com/geekstudio/rickandmorty/core/base/BaseDiffUtil.kt
@@ -4,7 +4,7 @@ import androidx.recyclerview.widget.DiffUtil
 
 class BaseDiffUtil<T : BaseDiffModel<S>, S> : DiffUtil.ItemCallback<T>() {
     override fun areItemsTheSame(oldItem: T, newItem: T) =
-        oldItem.nameService == newItem.nameService
+        oldItem.id == newItem.id
 
     override fun areContentsTheSame(oldItem: T, newItem: T) = oldItem == newItem
 }
