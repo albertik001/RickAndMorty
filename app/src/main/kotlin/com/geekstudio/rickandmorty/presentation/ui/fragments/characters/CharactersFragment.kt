aff402ca508d8ea02ad0ac7b5a1cb7a90a42531c
albertik001 <albert.nurlanovv@mail.ru>
2022-08-18 14:49:13 +0600

    displayed the data on the screen with the design

--- app/src/main/kotlin/com/geekstudio/rickandmorty/presentation/ui/fragments/characters/CharactersFragment.kt
+++ app/src/main/kotlin/com/geekstudio/rickandmorty/presentation/ui/fragments/characters/CharactersFragment.kt
@@ -1,10 +1,11 @@
 package com.geekstudio.rickandmorty.presentation.ui.fragments.characters
 
-import android.util.Log
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geekstudio.rickandmorty.R
 import com.geekstudio.rickandmorty.core.base.BaseFragment
+import com.geekstudio.rickandmorty.core.extensions.bindUIToLoadState
 import com.geekstudio.rickandmorty.databinding.FragmentCharactersBinding
 import com.geekstudio.rickandmorty.presentation.ui.adapters.CharactersPagingAdapter
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,19 +13,24 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class CharactersFragment :
     BaseFragment<FragmentCharactersBinding, CharactersViewModel>(R.layout.fragment_characters) {
+
     override val binding by viewBinding(FragmentCharactersBinding::bind)
     override val viewModel by viewModels<CharactersViewModel>()
     private val charactersAdapter = CharactersPagingAdapter()
 
-    override fun initialize() {
+    override fun initialize() = with(binding) {
+        binding.recyclerCharacters.layoutManager = LinearLayoutManager(context)
+        charactersAdapter.bindUIToLoadState(recyclerCharacters, progressCharactersLoader) {
+        }
+    }
 
+    override fun constructViews() {
+        binding.recyclerCharacters.adapter = charactersAdapter
     }
 
     override fun launchObservers() {
-        viewModel.fetchPagedCharacters().spectatePaging {
+        viewModel.fetchPagedCharacters().spectatePaging(success = {
             charactersAdapter.submitData(it)
-            Log.e("gaypop", it.toString())
-        }
-
+        })
     }
 }
