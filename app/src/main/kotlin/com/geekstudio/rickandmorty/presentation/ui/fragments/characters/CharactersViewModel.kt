073ddcbf639855304e6ebb1491ef165578fa4e93
albertik001 <albert.nurlanovv@mail.ru>
2022-08-18 14:50:49 +0600

    fix

--- app/src/main/kotlin/com/geekstudio/rickandmorty/presentation/ui/fragments/characters/CharactersViewModel.kt
+++ app/src/main/kotlin/com/geekstudio/rickandmorty/presentation/ui/fragments/characters/CharactersViewModel.kt
@@ -1,7 +1,5 @@
 package com.geekstudio.rickandmorty.presentation.ui.fragments.characters
 
-import androidx.lifecycle.viewModelScope
-import androidx.paging.cachedIn
 import com.geekstudio.rickandmorty.core.base.BaseViewModel
 import com.geekstudio.rickandmorty.domain.usecases.FetchPagedCharactersUseCase
 import com.geekstudio.rickandmorty.presentation.models.toUI
@@ -13,7 +11,8 @@ class CharactersViewModel @Inject constructor(
     private val fetchPagedCharactersUseCase: FetchPagedCharactersUseCase
 ) : BaseViewModel() {
 
-    fun fetchPagedCharacters() = fetchPagedCharactersUseCase().gatherPagingRequest { it.toUI() }
+    fun fetchPagedCharacters() =
+        fetchPagedCharactersUseCase().gatherPagingRequest { it.toUI() }
 
 
 }
