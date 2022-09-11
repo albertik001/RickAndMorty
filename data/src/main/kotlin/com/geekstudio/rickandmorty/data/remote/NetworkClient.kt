b66401d8ca9cb610115220de5ff7e66b902bc7e7
albertik001 <albert.nurlanovv@mail.ru>
2022-09-12 03:20:35 +0600

    написал provide EpisodeDao

--- data/src/main/kotlin/com/geekstudio/rickandmorty/data/remote/NetworkClient.kt
+++ data/src/main/kotlin/com/geekstudio/rickandmorty/data/remote/NetworkClient.kt
@@ -2,13 +2,15 @@ package com.geekstudio.rickandmorty.data.remote
 
 import com.geekstudio.rickandmorty.data.extensions.createAnApi
 import com.geekstudio.rickandmorty.data.remote.apiservices.CharactersApi
+import com.geekstudio.rickandmorty.data.remote.apiservices.EpisodesApi
 import javax.inject.Inject
 
 class NetworkClient @Inject constructor(
     retrofitClient: NetworkFastBuilder,
     okHttp: OkHttp
 ) {
+
     private val retrofit = retrofitClient.provideRetrofit(okHttp.provideOkHttpClient())
     fun provideCharactersApiService(): CharactersApi = retrofit.createAnApi()
-
+    fun provideEpisodesApiService(): EpisodesApi = retrofit.createAnApi()
 }
