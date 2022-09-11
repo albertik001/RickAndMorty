dfc19952ee34f84432f8c793c96be154ee63d3d9
albertik001 <albert.nurlanovv@mail.ru>
2022-09-12 03:12:22 +0600

    добавил методы для запроса всех персонажей, для одного и теже запросы но на базу данных

--- domain/src/main/kotlin/com/geekstudio/rickandmorty/domain/repository/CharactersRepository.kt
+++ domain/src/main/kotlin/com/geekstudio/rickandmorty/domain/repository/CharactersRepository.kt
@@ -1,9 +1,27 @@
 package com.geekstudio.rickandmorty.domain.repository
 
 import androidx.paging.PagingData
+import com.geekstudio.rickandmorty.domain.common.Either
 import com.geekstudio.rickandmorty.domain.models.CharactersModel
 import kotlinx.coroutines.flow.Flow
 
 interface CharactersRepository {
-    fun fetchPagedCharacters(): Flow<PagingData<CharactersModel>>
+
+    fun fetchPagedCharacters(
+        name: String?,
+        status: String?,
+        species: String?,
+        gender: String?
+    ): Flow<PagingData<CharactersModel>>
+
+    fun fetchSingleCharacter(id: Int): Flow<Either<String, CharactersModel>>
+
+    fun fetchLocalPagedCharacters(
+        name: String?,
+        status: String?,
+        species: String?,
+        gender: String?
+    ): Flow<List<CharactersModel>>
+
+    fun fetchLocalSingleCharacter(id: Int): Flow<CharactersModel>
 }
