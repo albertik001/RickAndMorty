c6341640a093b8666adac736aa62a2e2ba44656b
albertik001 <albert.nurlanovv@mail.ru>
2022-09-12 03:01:45 +0600

    удалил ненужные налл вопросительные и добавил маппинг на из модельки в дто

--- data/src/main/kotlin/com/geekstudio/rickandmorty/data/remote/dtos/CharactersDto.kt
+++ data/src/main/kotlin/com/geekstudio/rickandmorty/data/remote/dtos/CharactersDto.kt
@@ -1,66 +1,70 @@
 package com.geekstudio.rickandmorty.data.remote.dtos
 
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.geekstudio.rickandmorty.domain.models.*
 import com.google.gson.annotations.SerializedName
 
 data class RickAndMortyResponse<T>(
     @SerializedName("info")
-    val info: Info?,
+    val info: Info,
     @SerializedName("results")
-    val results: MutableList<T>
+    val results: List<T>
 )
 
 data class Info(
     @SerializedName("count")
-    val count: Int?,
+    val count: Int,
     @SerializedName("pages")
-    val pages: Int?,
+    val pages: Int,
     @SerializedName("next")
-    val next: String?,
+    val next: String,
     @SerializedName("prev")
-    val prev: Any?
+    val prev: Any
 )
 
 data class Location(
     @SerializedName("name")
-    val name: String?,
+    val name: String,
     @SerializedName("url")
-    val url: String?
+    val url: String
 )
 
 data class Origin(
     @SerializedName("name")
-    val name: String?,
+    val name: String,
     @SerializedName("url")
-    val url: String?
+    val url: String
 )
 
+@Entity
 data class CharactersDto(
     @SerializedName("id")
-    val id: Int?,
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
     @SerializedName("name")
-    val name: String?,
+    val name: String,
     @SerializedName("status")
-    val status: String?,
+    val status: String,
     @SerializedName("species")
-    val species: String?,
+    val species: String,
     @SerializedName("type")
-    val type: String?,
+    val type: String,
     @SerializedName("gender")
-    val gender: String?,
+    val gender: String,
     @SerializedName("origin")
-    val origin: Origin?,
+    val origin: Origin,
     @SerializedName("location")
-    val location: Location?,
+    val location: Location,
     @SerializedName("image")
-    val image: String?,
+    val image: String,
     @SerializedName("episode")
-    val episode: List<String?>?,
+    val episode: List<String>,
     @SerializedName("url")
-    val url: String?,
+    val url: String,
     @SerializedName("created")
-    val created: String?
+    val created: String,
 )
 
 fun CharactersDto.toDomain() = CharactersModel(
@@ -70,8 +74,8 @@ fun CharactersDto.toDomain() = CharactersModel(
     species,
     type,
     gender,
-    origin?.toDomain(),
-    location?.toDomain(),
+    origin.toDomain(),
+    location.toDomain(),
     image,
     episode,
     url,
@@ -85,4 +89,29 @@ fun Location.toDomain() = LocationModel(name, url)
 fun Info.toDomain() = InfoModel(count, pages, next, prev)
 
 fun RickAndMortyResponse<CharactersDto>.toDomain() =
-    RickAndMortyResponseModel(info?.toDomain(), results.map { it.toDomain() })
+    RickAndMortyResponseModel(info.toDomain(), results.map { it.toDomain() })
+
+fun CharactersModel.toDto() = CharactersDto(
+    id,
+    name,
+    status,
+    species,
+    type,
+    gender,
+    origin.toDto(),
+    location.toDto(),
+    image,
+    episode,
+    url,
+    created
+)
+
+fun OriginModel.toDto() = Origin(name, url)
+
+fun LocationModel.toDto() = Location(name, url)
+
+fun InfoModel.toDto() = Info(count, pages, next, prev)
+
+fun RickAndMortyResponseModel.toDto() =
+    RickAndMortyResponse(info.toDto(), results.map { it.toDto() })
+
