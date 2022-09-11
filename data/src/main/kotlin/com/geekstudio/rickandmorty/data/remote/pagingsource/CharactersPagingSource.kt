3bc4a576f10157ceb01d05a610a32e0d95edae69
albertik001 <albert.nurlanovv@mail.ru>
2022-09-12 03:11:11 +0600

    передал в конструктор параметры для фильтрации с пагинацией

--- data/src/main/kotlin/com/geekstudio/rickandmorty/data/remote/pagingsource/CharactersPagingSource.kt
+++ data/src/main/kotlin/com/geekstudio/rickandmorty/data/remote/pagingsource/CharactersPagingSource.kt
@@ -8,20 +8,24 @@ import com.geekstudio.rickandmorty.data.remote.dtos.CharactersDto
 import okio.IOException
 import retrofit2.HttpException
 
-class CharactersPagingSource(private val charactersApi: CharactersApi) :
-    PagingSource<Int, CharactersDto>() {
+class CharactersPagingSource(
+    private val charactersApi: CharactersApi,
+    private val name: String?,
+    private val status: String?,
+    private val species: String?,
+    private val gender: String?
+) : PagingSource<Int, CharactersDto>() {
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, CharactersDto> {
         val page = params.key ?: 1
         return try {
-            val response = charactersApi.fetchCharacters(page)
+            val response = charactersApi.fetchCharacters(page, name, status, species, gender)
 
             val nextPageNumber = if (response.info?.next == null) {
                 null
             } else
                 Uri.parse(response.info.next).getQueryParameter("page")?.toInt()
             LoadResult.Page(data = response.results, prevKey = null, nextKey = nextPageNumber)
-
 
         } catch (ioException: IOException) {
             LoadResult.Error(ioException)
@@ -30,7 +34,6 @@ class CharactersPagingSource(private val charactersApi: CharactersApi) :
         } catch (exception: Exception) {
             LoadResult.Error(exception)
         }
-
     }
 
     override fun getRefreshKey(state: PagingState<Int, CharactersDto>): Int? {
@@ -40,5 +43,4 @@ class CharactersPagingSource(private val charactersApi: CharactersApi) :
 
         }
     }
-
 }
