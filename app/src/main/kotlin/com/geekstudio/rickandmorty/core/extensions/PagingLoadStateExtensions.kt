a342d8bc3762d174dac89990d3f70c6cf157fd68
albertik001 <albert.nurlanovv@mail.ru>
2022-09-12 03:22:07 +0600

    переписал и добавил +1 функцию

--- app/src/main/kotlin/com/geekstudio/rickandmorty/core/extensions/PagingLoadStateExtensions.kt
+++ app/src/main/kotlin/com/geekstudio/rickandmorty/core/extensions/PagingLoadStateExtensions.kt
@@ -1,5 +1,6 @@
 package com.geekstudio.rickandmorty.core.extensions
 
+import android.view.View
 import android.widget.ProgressBar
 import androidx.core.view.isVisible
 import androidx.paging.CombinedLoadStates
@@ -10,11 +11,16 @@ import androidx.recyclerview.widget.RecyclerView
 fun <T : Any, VH : RecyclerView.ViewHolder> PagingDataAdapter<T, VH>.bindUIToLoadState(
     recyclerView: RecyclerView,
     progressBar: ProgressBar,
-    listener: (CombinedLoadStates) -> Unit
+    view: View? = null,
+    listener: ((CombinedLoadStates) -> Unit)? = null
 ) {
     addLoadStateListener { loadStates ->
         recyclerView.isVisible = loadStates.refresh is LoadState.NotLoading
         progressBar.isVisible = loadStates.refresh is LoadState.Loading
-        listener(loadStates)
+        view?.isVisible = loadStates.refresh !is LoadState.Loading
+        if (recyclerView.isVisible) {
+            view?.isVisible = false
+        }
+        listener?.invoke(loadStates)
     }
 }
