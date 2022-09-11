6fadf63baad502b00bdce3f0275a4b25fe7f253c
albertik001 <albert.nurlanovv@mail.ru>
2022-09-12 03:13:32 +0600

    Сделала модельку not-null

--- app/src/main/kotlin/com/geekstudio/rickandmorty/presentation/models/CharactersUI.kt
+++ app/src/main/kotlin/com/geekstudio/rickandmorty/presentation/models/CharactersUI.kt
@@ -4,40 +4,41 @@ import com.geekstudio.rickandmorty.core.base.BaseDiffModel
 import com.geekstudio.rickandmorty.domain.models.*
 
 data class RickAndMortyResponseUI(
-    val info: InfoUI?,
+    val info: InfoUI,
     val results: List<CharactersUI>
 )
 
 data class InfoUI(
-    val count: Int?,
-    val pages: Int?,
-    val next: String?,
-    val prev: Any?
+    val count: Int,
+    val pages: Int,
+    val next: String,
+    val prev: Any
 )
 
 data class LocationUI(
-    val name: String?,
-    val url: String?
+    val name: String,
+    val url: String
 )
 
 data class OriginUI(
-    val name: String?,
-    val url: String?
+    val name: String,
+    val url: String
 )
 
 data class CharactersUI(
-    override val id: Int?,
-    val name: String?,
-    val status: String?,
-    val species: String?,
-    val type: String?,
-    val gender: String?,
-    val origin: OriginUI?,
-    val location: LocationUI?,
-    val image: String?,
-    val episode: List<String?>?,
-    val url: String?,
-    val created: String?
+    override val id: Int,
+    val name: String,
+    val status: String,
+    val species: String,
+    val type: String,
+    val gender: String,
+    val origin: OriginUI,
+    val location: LocationUI,
+    val image: String,
+    val episode: List<String>,
+    val url: String,
+    val created: String,
+    var firstSeenIn: String = ""
 ) : BaseDiffModel<Int>
 
 fun CharactersModel.toUI() = CharactersUI(
@@ -47,8 +48,8 @@ fun CharactersModel.toUI() = CharactersUI(
     species,
     type,
     gender,
-    origin?.toUI(),
-    location?.toUI(),
+    origin.toUI(),
+    location.toUI(),
     image,
     episode,
     url,
@@ -62,4 +63,28 @@ fun LocationModel.toUI() = LocationUI(name, url)
 fun InfoModel.toUI() = InfoUI(count, pages, next, prev)
 
 fun RickAndMortyResponseModel.toUI() =
-    RickAndMortyResponseUI(info?.toUI(), results.map { it.toUI() })
+    RickAndMortyResponseUI(info.toUI(), results.map { it.toUI() })
+
+fun CharactersUI.toModel() = CharactersModel(
+    id,
+    name,
+    status,
+    species,
+    type,
+    gender,
+    origin.toModel(),
+    location.toModel(),
+    image,
+    episode,
+    url,
+    created
+)
+
+fun OriginUI.toModel() = OriginModel(name, url)
+
+fun LocationUI.toModel() = LocationModel(name, url)
+
+fun InfoUI.toModel() = InfoModel(count, pages, next, prev)
+
+fun RickAndMortyResponseUI.toModel() =
+    RickAndMortyResponseModel(info.toModel(), results.map { it.toModel() })
