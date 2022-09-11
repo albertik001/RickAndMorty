2d00eb6131e5e7ca243792e9f4733ae8620f8ff1
albertik001 <albert.nurlanovv@mail.ru>
2022-09-12 03:10:32 +0600

    написад метод который по статусу опеределяет и красит кружок в item на тот цвет который приходит с бекенда, метод который оформляет запрос на последнее местонахождения персонажа, клик на item с передачей его id

--- app/src/main/kotlin/com/geekstudio/rickandmorty/presentation/ui/adapters/CharactersPagingAdapter.kt
+++ app/src/main/kotlin/com/geekstudio/rickandmorty/presentation/ui/adapters/CharactersPagingAdapter.kt
@@ -2,15 +2,21 @@ package com.geekstudio.rickandmorty.presentation.ui.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.geekstudio.rickandmorty.R
 import com.geekstudio.rickandmorty.core.base.BaseDiffUtil
-import com.geekstudio.rickandmorty.core.extensions.loadImageWithGlide
+import com.geekstudio.rickandmorty.core.extensions.setImage
+import com.geekstudio.rickandmorty.core.extensions.setOnSingleClickListener
 import com.geekstudio.rickandmorty.databinding.ItemCharacterBinding
+import com.geekstudio.rickandmorty.presentation.enums.CharacterStatus
 import com.geekstudio.rickandmorty.presentation.models.CharactersUI
 
-class CharactersPagingAdapter :
+class CharactersPagingAdapter(
+    private val itemCLick: (id: Int?) -> Unit,
+    val fetchFirstSeenIn: (position: Int, episodeUrl: String) -> Unit,
+) :
     PagingDataAdapter<CharactersUI, CharactersPagingAdapter.CharactersViewHolder>(BaseDiffUtil()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = CharactersViewHolder(
         ItemCharacterBinding.inflate(
@@ -22,17 +28,52 @@ class CharactersPagingAdapter :
         getItem(position)?.let { holder.onBind(it) }
     }
 
-    class CharactersViewHolder(private val binding: ItemCharacterBinding) :
+    fun setFirstSeenIn(position: Int, firstSeenIn: String) {
+        getItem(position)?.firstSeenIn = firstSeenIn
+        notifyItemChanged(position)
+    }
+
+    inner class CharactersViewHolder(private val binding: ItemCharacterBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun onBind(character: CharactersUI) {
-            binding.textItemCharacterFirstSeenInData.isSelected = true
-            binding.imageItemCharacter.loadImageWithGlide(character.image)
-            binding.textItemCharacterName.text = character.name
-            binding.textItemCharacterFirstSeenInData.text = character.created
-            binding.textItemCharacterLastKnownLocationData.text = character.location?.name
-            binding.textItemCharacterStatusAndSpecies.text =
-                binding.imageItemCharacterStatus.context
-                    .resources.getString(R.string.hyphen, character.status, character.species)
+        init {
+            itemView.setOnSingleClickListener {
+                itemCLick(getItem(absoluteAdapterPosition)?.id)
+            }
+        }
+
+        fun onBind(character: CharactersUI) = with(binding) {
+            textItemCharacterFirstSeenInData.isSelected = true
+            imageItemCharacter.setImage(character.image)
+            textItemCharacterName.text = character.name
+            textItemCharacterLastKnownLocationData.text = character.location.name
+            textItemCharacterStatusAndSpecies.text = imageItemCharacterStatus.context
+                .resources.getString(R.string.hyphen, character.status, character.species)
+            setupFirstSeenIn(character.firstSeenIn, character.episode.first())
+            setupCharacterStatus(character.status)
+        }
+
+        private fun setupCharacterStatus(status: String) = with(binding) {
+            when (status) {
+                CharacterStatus.ALIVE.status -> {
+                    imageItemCharacterStatus.setImageResource(CharacterStatus.ALIVE.image)
+                }
+                CharacterStatus.DEAD.status -> {
+                    imageItemCharacterStatus.setImageResource(CharacterStatus.DEAD.image)
+                }
+                CharacterStatus.UNKNOWN.status -> {
+                    imageItemCharacterStatus.setImageResource(CharacterStatus.UNKNOWN.image)
+                }
+            }
+        }
+
+        private fun setupFirstSeenIn(firstSeenIn: String, episode: String) = with(binding) {
+            progressBarCharacterFirstSeenIn.isVisible = firstSeenIn.isEmpty()
+            textItemCharacterFirstSeenInData.isVisible = firstSeenIn.isNotEmpty()
+            if (firstSeenIn.isEmpty()) {
+                fetchFirstSeenIn(absoluteAdapterPosition, episode)
+            } else {
+                textItemCharacterFirstSeenInData.text = firstSeenIn
+            }
         }
     }
 }
